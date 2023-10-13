a31a327ec0be9e4bcf6f02a41ced2c1d993bcada
binyamolango <olangobinyam@gmail.com>
2023-10-13 18:19:04 +0300

    Update line ending

---
+++ decode_method.rb
@@ -0,0 +1,44 @@
+CODE = {
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+  '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+  '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+  '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+  '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z',
+  '.----' => 1, '..---' => 2, '...--' => 3, '....-' => 4, '.....' => 5,
+  '-....' => 6, '--...' => 7, '---..' => 8, '----.' => 9, '-----' => 0
+}.freeze
+
+def decode_char(char)
+  CODE[char]
+end
+
+# result = decode_char("--")
+# puts result
+
+def decode_word(word)
+  chars = word.split
+  char_decode = ''
+
+  chars.each do |char|
+    char_decode += decode_char(char)
+  end
+
+  char_decode
+end
+
+# result = decode_word("-- -.--")
+# puts result
+
+def decode(sentence)
+  words = sentence.split('   ')
+  word_decode = ''
+
+  words.each do |word|
+    word_decode += "#{decode_word(word)} "
+  end
+
+  word_decode
+end
+
+result = decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts result
